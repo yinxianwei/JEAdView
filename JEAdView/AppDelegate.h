ae8e2a0134f052f38100bcc24d6d502efb9cd037
尹现伟 <ios@yinxianwei.cn>
2015-03-25 14:23:43 +0800

    Initial Commit

---
+++ JEAdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JEAdView
+//
+//  Created by 尹现伟 on 15/3/25.
+//  Copyright (c) 2015年 上海美问信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
